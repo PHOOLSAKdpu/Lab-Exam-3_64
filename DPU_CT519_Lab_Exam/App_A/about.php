434235d04d5067bcb28670ed96140275791cc645
PHOOLSAKdpu <645162010005@dpu.ac.th>
2022-08-15 05:16:20 +0000

    first commit

---
+++ DPU_CT519_Lab_Exam/App_A/about.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>about</title>
+</head>
+
+<body>
+    <img src="https://github.com/PHOOLSAKdpu/DPU_CT519_Lab4/blob/main/mypicc.jpg?raw=true" width="180px" height="200px" />
+
+    <img src="https://github.com/PHOOLSAKdpu/DPU_CT519_Lab4/blob/main/mypic2.jpg.png?raw=true" width="180px" height="200px" />
+
+    <img src="https://github.com/PHOOLSAKdpu/DPU_CT519_Lab4/blob/main/mypic.jpg?raw=true" width="180px" height="200px" />
+    <h1>PHOOLSAK JANTARAMANEE</h1>
+    <h1>645162010005</h1>
+    <div>
+        <p>กลับสู่หน้า sql </p</p>
+        <a href="index.php">click to see sql</a>
+    </div>
+</body>
+
+</html>
+
